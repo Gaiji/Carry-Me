be878795581653934268867088d590899cf06e77
Gaiji <34226052+Gaiji@users.noreply.github.com>
2017-12-04 15:27:06 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ client.on('message', message => {
     }
     snekfetch.get(api).then(r => {
 	let body = r.body;
-	let id = 40
+	let id = Number(10);
 	let entry = body.find(post => post.id === id);
 	console.log(entry);
     });
