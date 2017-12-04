e50ce9f3891c35dbd5d80513b4460bbe9323ed9a
Gaiji <34226052+Gaiji@users.noreply.github.com>
2017-12-05 02:20:57 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,6 +28,9 @@ client.on('message', message => {
 	var url ='https://api.mymemory.translated.net/get?q='+ unk +'&langpair=en|ja'
 	message.channel.send(url);
 	request(url, function(err, response, body) {
+	    if(!body) {
+                return message.reply('指定されたプレイヤーは存在しません');
+            }
 	    body = JSON.parse(body);
 	    message.channel.send(body.responseData.translatedText);
 	});
