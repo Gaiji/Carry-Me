ab916ccfb1bac71802ecf63686ebb3425e0b0cab
Gaiji <34226052+Gaiji@users.noreply.github.com>
2017-12-04 15:16:05 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on('message', message => {
     	message.author.send("コマンドリスト: ");
 	message.author.send(";ping - あなたのPingを表示します");
     }
-    snekfetch.get(api).then(message.author.send);
+    snekfetch.get(api).then(console.log);
 });
 
 client.login(process.env.BOT_TOKEN);
