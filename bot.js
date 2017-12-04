c7d44b767c60be2f964b59759eacef15934aedec
Gaiji <34226052+Gaiji@users.noreply.github.com>
2017-12-04 13:40:42 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-let prefix = '@'
+let prefix = '!'
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -8,9 +8,6 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === prefix + 'ping') {
-    	message.reply('pong');
-  	}
-    if (message.content === prefix + 'test') {
     	message.reply('pong');
   	}
 });
