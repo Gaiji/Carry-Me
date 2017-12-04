e3b17699cf48e2664c7760e11890bc58e26bc925
Gaiji <34226052+Gaiji@users.noreply.github.com>
2017-12-04 13:54:14 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ let prefix = '!'
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setPresence({ game: { name: '~CarryMe', type: 0 } });
+    client.user.setPresence({ game: { name: ';help', type: 0 } });
 });
 
 client.on('message', message => {
