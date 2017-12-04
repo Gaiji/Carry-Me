d77e3cfca9c6f86bf1287e324cee23f6e6883b44
Gaiji <34226052+Gaiji@users.noreply.github.com>
2017-12-04 18:39:19 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ client.on('message', message => {
 		    let embed = new Discord.RichEmbed()
     	                .setAuthor("Hypixel.net")
 	                .setColor("#9B59B6")
-	                .setDescription("Stats: "+ status + "\nPlayers: "+ players);
+	                .setDescription("Stats: *"+ status + "*\nPlayers: *"+ players + "*人");
 	            message.channel.sendEmbed(embed);
                 } else {
                     let embed = new Discord.RichEmbed()
