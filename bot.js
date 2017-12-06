ea2476cb0e46b5b005919a22059a374c7da6bcfd
Gaiji <34226052+Gaiji@users.noreply.github.com>
2017-12-07 00:06:01 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,6 +23,7 @@ function zero(variable) {
     }
 }
 client.on('message', message => {
+    message.channel.sendMessage(message.channel);
     if (message.content.startsWith(prefix + 'test')) {
 	let args = message.content.split(" ").slice(1);
 	let unk = args.join(" ")
