18d15fd9b2c6e67e76dd61277eb0caab792566aa
Gaiji <34226052+Gaiji@users.noreply.github.com>
2017-12-06 19:40:16 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const snekfetch = require("snekfetch");
-const mc = require('minecraft-protocol');
 
 let prefix = ';'
 
